7023a4da86f34d739597166b4ce78c2a6971a834
Warren Pieterse <warren.pieterse@gmail.com>
2022-09-14 19:24:49 +0200

    .WriteTo.Elasticsearch(options)

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Extensions.Logging;
+using Serilog.Sinks.Elasticsearch;
+using System;
 
 [assembly: FunctionsStartup(typeof(AzureFunctionDemo.Startup))]
 
@@ -13,10 +15,20 @@ public class Startup : FunctionsStartup
 {
     public override void Configure(IFunctionsHostBuilder builder)
     {
+        ElasticsearchSinkOptions options = new ElasticsearchSinkOptions(new Uri("http://elastic:yPlOLBzwB0AvLLVDh1Kt@picup-elknew-stack.eastus2.cloudapp.azure.com:9220/"))
+        {
+            IndexFormat = "routing-warren",
+            AutoRegisterTemplate = true,
+            //ModifyConnectionSettings = (c) => c.BasicAuthentication("yyy", "zzz"),
+            //NumberOfShards = 2,
+            //NumberOfReplicas = 0
+        };
+
         builder.Services.AddSingleton<ILoggerProvider>((sp) =>
         {
             Log.Logger = new LoggerConfiguration()
                 .Enrich.FromLogContext()
+                .WriteTo.Elasticsearch(options)
                 //.WriteTo.ApplicationInsights(sp.GetRequiredService<TelemetryClient>(), TelemetryConverter.Traces)
                 .CreateLogger();
 
