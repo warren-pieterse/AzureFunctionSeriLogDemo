ad8152d6115363bbec28f3fed2e12373bdb69afa
Warren Pieterse <warren.pieterse@gmail.com>
2022-09-14 18:20:26 +0200

    init commit

---
+++ Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+[assembly: FunctionsStartup(typeof(AzureFunctionDemo.Startup))]
+
+
+namespace AzureFunctionDemo;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddSingleton<ILoggerProvider>((sp) =>
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                //.WriteTo.ApplicationInsights(sp.GetRequiredService<TelemetryClient>(), TelemetryConverter.Traces)
+                .CreateLogger();
+
+            return new SerilogLoggerProvider(Log.Logger, true);
+        });
+    }
+}
